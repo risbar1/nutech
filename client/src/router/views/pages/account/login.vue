d87af7fdaef61914cf957ccbbed8d110b88de730
risbar1 <yaniragil65@gmail.com>
2024-10-12 06:19:43 +0700

    tampilan register

--- client/src/router/views/pages/account/login.vue
+++ client/src/router/views/pages/account/login.vue
@@ -14,8 +14,8 @@ export default {
 	components: { Layout },
 	data() {
 		return {
-			username: 'admin',
-			password: 'password',
+			email: '',
+			password: '',
 			authError: null,
 			tryingToLogIn: false,
 			isAuthError: false,
@@ -26,7 +26,7 @@ export default {
 			return process.env.NODE_ENV === 'production'
 				? {}
 				: {
-						username: 'Use "admin" to log in with the mock API',
+						email: 'Use "admin" to log in with the mock API',
 						password: 'Use "password" to log in with the mock API',
 				  }
 		},
@@ -74,18 +74,13 @@ export default {
 									<div class="col-md-6 p-5">
 										<div class="mx-auto mb-5">
 											<a routerLink="/">
-												<img src="@assets/images/logo.png" alt height="24" />
+												<img src="https://nutech-integrasi.com/wp-content/uploads/2019/09/Logo-Nutech-ok.png" alt height="24" />
 												<h3 class="d-inline align-middle ml-1 text-logo"
-													>Shreyu</h3
+													>Contract SIMS PPOB</h3
 												>
 											</a>
 										</div>
 
-										<h6 class="h5 mb-0 mt-4">Welcome back!</h6>
-										<p class="text-muted mt-1 mb-4">
-											Enter your email address and password to access admin
-											panel.
-										</p>
 
 										<b-alert
 											v-model="isAuthError"
@@ -99,6 +94,7 @@ export default {
 											@submit.prevent="tryToLogIn"
 										>
 											<div class="form-group">
+												<label class="form-control-label">Email</label>
 												<div class="input-group input-group-merge">
 													<div class="input-group-prepend">
 														<span class="input-group-text">
@@ -110,20 +106,15 @@ export default {
 													</div>
 													<b-form-input
 														id="input-1"
-														v-model="username"
+														v-model="email"
 														type="text"
 														required
-														placeholder="Enter username"
+														placeholder="Enter email"
 													></b-form-input>
 												</div>
 											</div>
 											<div class="form-group mt-4">
 												<label class="form-control-label">Password</label>
-												<router-link
-													to="forget-password"
-													class="float-right text-muted text-unline-dashed ml-1"
-													>Forgot your password?</router-link
-												>
 												<div class="input-group input-group-merge">
 													<div class="input-group-prepend">
 														<span class="input-group-text">
@@ -142,22 +133,6 @@ export default {
 													></b-form-input>
 												</div>
 											</div>
-											<div class="form-group mb-4">
-												<div class="custom-control custom-checkbox">
-													<input
-														id="checkbox-signin"
-														type="checkbox"
-														class="custom-control-input"
-														checked
-													/>
-													<label
-														class="custom-control-label"
-														for="checkbox-signin"
-													>
-														Remember me
-													</label>
-												</div>
-											</div>
 											<b-form-group id="button-group" class="mt-4 mb-1">
 												<b-button
 													type="submit"
@@ -166,36 +141,19 @@ export default {
 													>Log In</b-button
 												>
 											</b-form-group>
-										</b-form>
-										<div class="py-3 text-center">
-											<span class="font-size-16 font-weight-bold">Or</span>
-										</div>
-										<div class="row">
-											<div class="col-6">
-												<a href="javascript: void(0);" class="btn btn-white">
-													<i class="uil uil-google icon-google mr-2"></i>With
-													Google
-												</a>
-											</div>
-											<div class="col-6 text-right">
-												<a href="javascript: void(0);" class="btn btn-white">
-													<i class="uil uil-facebook mr-2 icon-fb"></i>With
-													Facebook
-												</a>
-											</div>
-										</div>
+										</b-form><br><br><br>
 									</div>
 									<div class="col-lg-6 d-none d-md-inline-block">
 										<div class="auth-page-sidebar">
 											<div class="overlay"></div>
 											<div class="auth-user-testimonial">
 												<p class="font-size-24 font-weight-bold text-white mb-1"
-													>I simply love it!</p
+													>Introduction</p
 												>
 												<p class="lead"
-													>"It's a elegent templete. I love it very much!"</p
+													>PT. Nutech Integrasi merupakan bagian dari TelkomGroup, berkomitmen menjadikan Indonesia sebagai pusat inovasi digital terbaik di Asia Tenggara serta secara efisien menghasilkan kualitas terbaik berdasarkan pengalaman pelanggan.</p
 												>
-												<p>- Admin User</p>
+												<p>- Management Office</p>
 											</div>
 										</div>
 									</div>
@@ -208,13 +166,13 @@ export default {
 						<div class="row mt-3">
 							<div class="col-12 text-center">
 								<p class="text-muted">
-									Don't have an account?
+									Apakah Anda Memiliki Akun ?
 									<router-link
 										tag="a"
 										to="/register"
 										class="text-primary font-weight-bold ml-1"
 									>
-										<b>Sign Up</b>
+										<b>Daftar</b>
 									</router-link>
 									<!-- <a
                     routerLink="/account/signup"
--- client/src/router/views/pages/account/register.vue
+++ client/src/router/views/pages/account/register.vue
@@ -66,21 +66,21 @@ export default {
 									<div class="col-lg-6 p-5">
 										<div class="mx-auto mb-5">
 											<a href="/">
-												<img src="@assets/images/logo.png" alt height="24" />
+												<img src="https://nutech-integrasi.com/wp-content/uploads/2019/09/Logo-Nutech-ok.png" alt height="24" />
 												<h3 class="d-inline align-middle ml-1 text-logo"
-													>Shreyu</h3
+													>Contract SIMS PPOB</h3
 												>
 											</a>
 										</div>
 
-										<h6 class="h5 mb-0 mt-4">Create your account</h6>
+										<h6 class="h5 mb-0 mt-4">Daftar</h6>
 										<p class="text-muted mt-0 mb-4"
-											>Create a free account and start using Shreyu</p
+											>Buat akun gratis dan mulai gunakan Contract SIMS PPOB</p
 										>
 
 										<form action="#" class="authentication-form">
 											<div class="form-group">
-												<label class="form-control-label">Name</label>
+												<label class="form-control-label">Nama Awal</label>
 												<div class="input-group input-group-merge">
 													<div class="input-group-prepend">
 														<span class="input-group-text">
@@ -91,7 +91,25 @@ export default {
 														id="name"
 														type="text"
 														class="form-control"
-														placeholder="Your full name"
+														placeholder="Nama Awal"
+														required
+													/>
+												</div>
+											</div>
+
+											<div class="form-group">
+												<label class="form-control-label">Nama Akhir</label>
+												<div class="input-group input-group-merge">
+													<div class="input-group-prepend">
+														<span class="input-group-text">
+															<feather type="user" class="icon-dual"></feather>
+														</span>
+													</div>
+													<input
+														id="name"
+														type="text"
+														class="form-control"
+														placeholder="Nama Akhir"
 														required
 													/>
 												</div>
@@ -109,7 +127,7 @@ export default {
 														id="email"
 														type="email"
 														class="form-control"
-														placeholder="hello@coderthemes.com"
+														placeholder="hello@nutech-integrasi.com"
 													/>
 												</div>
 											</div>
@@ -139,21 +157,12 @@ export default {
 														class="custom-control-input"
 														checked
 													/>
-													<label
-														class="custom-control-label"
-														for="checkbox-signup"
-													>
-														I accept
-														<a href="javascript: void(0);"
-															>Terms and Conditions</a
-														>
-													</label>
 												</div>
 											</div>
 
 											<div class="form-group mb-0 text-center">
 												<button class="btn btn-primary btn-block" type="submit"
-													>Sign Up</button
+													>Daftar</button
 												>
 											</div>
 										</form>
@@ -164,12 +173,12 @@ export default {
 											<div class="overlay"></div>
 											<div class="auth-user-testimonial">
 												<p class="font-size-24 font-weight-bold text-white mb-1"
-													>I simply love it!</p
+													>Management Office</p
 												>
 												<p class="lead"
-													>"It's a elegent templete. I love it very much!"</p
+													>Gedung Nutech, Jl. Buncit Raya No.Kav. 99, RT.1/RW.7, Pejaten Barat, Ps. Minggu, Kota Jakarta Selatan, Daerah Khusus Ibukota Jakarta 12510
+													T : +6221 27808111</p
 												>
-												<p>- Admin User</p>
 											</div>
 										</div>
 									</div>
@@ -182,7 +191,7 @@ export default {
 						<div class="row mt-3">
 							<div class="col-12 text-center">
 								<p class="text-muted">
-									Already have account?
+									Sudah punya akun ?
 									<router-link
 										tag="a"
 										to="/login"
