1b9be9bdaac4875428471dd430b75c5d28335f27
risbar1 <yaniragil65@gmail.com>
2024-10-12 09:44:02 +0700

    psw minimal 8

--- server/src/module/login/login.controller.ts
+++ server/src/module/login/login.controller.ts
@@ -2,8 +2,8 @@ require("dotenv").config();
 import { Request, Response } from "express";
 import axios from 'axios';
 import WsModel from "./model";
-import bcrypt = require("bcryptjs");
-import jwt = require("jsonwebtoken");
+import bcrypt from  "bcryptjs";
+import jwt from "jsonwebtoken";
 
 export const LoginController = async (
   req: Request,
@@ -15,18 +15,27 @@ export const LoginController = async (
         where: {email}
     });
     if (!user) {
-        res.status(401).json({
+      return res.status(401).json({
           status: "103",
           message:'Email tidak ditemukan',
           data:null
         });
     }
 
+    // minimal 8
+    if (password.length<8) {
+      return res.status(401).json({
+          status: "103",
+          message:'Parameter request password Length minimal 8 karakter',
+          data:null
+        });
+    }
+    // minimal 8
 
     // Verify password
     const passwordValid = await bcrypt.compare(password, user?.dataValues.password);
     if (!passwordValid) {
-        res.status(401).json({
+      return res.status(401).json({
           status: "103",
           message:'Email atau password salah',
           data:null
@@ -47,7 +56,7 @@ export const LoginController = async (
     });
 
   } catch (error: any) {
-    res.status(500).json({
+    res.status(400).json({
       status: "error",
       message: error.message,
     });
@@ -60,16 +69,36 @@ export const RegistrasiController = async (
 ) => {
   try {
     const { first_name, last_name, email, password } = req.body;
+    // email karakter gak sesuai
+    const emailToValidate = email;
+    const emailRegexp = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;
+
+    if (emailRegexp.test(emailToValidate) == false) {
+      return res.status(400).json({
+        status: "102",
+        message:'Paramter email tidak sesuai format',
+        data: null
+      });
+    }
+
+    // minimal 8
+    if (password.length<8) {
+      return res.status(401).json({
+          status: "103",
+          message:'Parameter request password Length minimal 8 karakter',
+          data:null
+        });
+    }
+    // minimal 8
+
     // Check if the email exists
     const userExists = await WsModel.findOne({
         where: {email}
     });
-    if (userExists) {
-        res.status(400).json({
-          status: "102",
+    if (userExists) {   
+        return res.status(400).json({status: "102",
           message:'Email sudah ada',
-          data: null
-        });
+          data: null});  
     }
 
     await WsModel.create({
@@ -85,7 +114,7 @@ export const RegistrasiController = async (
       data: null
     });
 } catch (err) {
-    res.status(200).json({
+    res.status(500).json({
       status: "500",
       message:'Error in registering user',
     });
