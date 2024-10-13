3865a7608cbc062db785f8744784bf97ede07c08
risbar1 <yaniragil65@gmail.com>
2024-10-13 22:50:21 +0700

    update profile vue

--- client/src/router/views/pages/dashboard/profile.vue
+++ client/src/router/views/pages/dashboard/profile.vue
@@ -24,6 +24,10 @@ export default {
 		return {
 		api: Utils.Host(),
 		dataprofile:[],
+		dataupdate:false,
+		databiodata:true,
+		alert_berhasil:false,
+		message:""
 		}
 	},
 	mounted() {
@@ -31,12 +35,44 @@ export default {
       this.GetBanner()
     },
 	methods: {
+		async Gantifoto() {
+		try {
+			this.$router.push("/profile");
+		} catch (err) {
+			console.error(err);
+		}
+		},
+		async Gantibiodata() {
+		try {
+			this.dataupdate = true
+			this.databiodata = false
+		} catch (err) {
+			console.error(err);
+		}
+		},
 		async GetBanner() {
 		try {
 			let response = await axios.get(this.api+"/halaman/profile", Utils.authHeader());
 			this.dataprofile = response.data.data[0];
 		} catch (err) {
 			console.error(err);
+		}
+		},
+		async Updatebiodata() {
+		try {
+			const dataisi = new URLSearchParams();
+			dataisi.append("first_name", this.dataprofile.first_name);
+			dataisi.append("last_name", this.dataprofile.last_name);
+			dataisi.append("hp", this.dataprofile.hp);
+			dataisi.append("alamat", this.dataprofile.alamat);
+			let response = await axios.put(this.api+"/halaman/profile", dataisi, Utils.authHeader());
+			if (response.data.status === '0'){
+				this.alert_berhasil = true
+				this.message =  response.data.message
+			} 
+		} catch (err) {
+			this.alert = true
+			this.message =  err.response.data.message
 		}
 		},
 	},
@@ -63,18 +99,25 @@ export default {
 				/>
 				<h5 class="mt-2 mb-0">{{ dataprofile.first_name }} {{ dataprofile.last_name }}</h5><br>
 
-				<button type="button" class="btn btn-primary btn-sm mr-1"
-					>Ganti Foto</button
+				<button type="button" class="btn btn-primary btn-sm mr-1" @click="Gantifoto">Ganti Foto</button
 				>
-				<button type="button" class="btn btn-white btn-sm ml-1">Ganti Biodata</button>
+				<button type="button" class="btn btn-white btn-sm ml-1" @click="Gantibiodata">Ganti Biodata</button>
 			</div>
+
+				<br>
+			<b-alert
+			v-model="alert_berhasil"
+			variant="success"
+			dismissible
+			>{{ message }}</b-alert
+		>
 
 			<!-- profile  -->
 			<div class="mt-3 pt-2 border-top">
 				<h4 class="mb-3 font-size-15">Contact Information</h4>
 				<div class="table-responsive">
 					<table class="table table-borderless mb-0 text-muted">
-						<tbody>
+						<tbody v-if="databiodata === true">
 							<tr>
 								<th scope="row">Email</th>
 								<td>{{ dataprofile.email }}</td>
@@ -87,6 +130,68 @@ export default {
 								<th scope="row">Address</th>
 								<td>
 									{{ dataprofile.alamat }}
+								</td>
+							</tr>
+						</tbody>
+						<tbody v-if="dataupdate === true">
+							<tr>
+								<th scope="row">Email</th>
+								<td>{{ dataprofile.email }}
+								</td>
+							</tr>
+							<tr>
+								<th scope="row">Phone</th>
+								<td>
+									<b-form-input
+									id="input-1"
+									v-model="dataprofile.hp"
+									type="text"
+									required
+									placeholder="Enter hp"
+								></b-form-input>
+								</td>
+							</tr>
+							<tr>
+								<th scope="row">Address</th>
+								<td>
+									<b-form-input
+									id="input-1"
+									v-model="dataprofile.alamat"
+									type="text"
+									required
+									placeholder="Enter alamat"
+								></b-form-input>
+								</td>
+							</tr>
+							<tr>
+								<th scope="row">Nama Awal</th>
+								<td>
+									<b-form-input
+									id="input-1"
+									v-model="dataprofile.first_name"
+									type="text"
+									required
+									placeholder="Enter Nama Awal"
+								></b-form-input>
+								</td>
+							</tr>
+							<tr>
+								<th scope="row">Nama Akhir</th>
+								<td>
+									<b-form-input
+									id="input-1"
+									v-model="dataprofile.last_name"
+									type="text"
+									required
+									placeholder="Enter Nama Akhir"
+								></b-form-input>
+								</td>
+							</tr>
+							<tr>
+								<td colspan="2">
+									<button class="btn btn-primary btn-block" @click="Updatebiodata"
+										>Update Biodata</button
+									>
 								</td>
 							</tr>
 						</tbody>
